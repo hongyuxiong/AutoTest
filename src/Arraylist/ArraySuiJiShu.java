5afc638d2c712664e23701b33497eb9b208f5416
hongyuxiong <843409677@qq.com>
2021-04-15 08:11:29 +0800

    数组中求最大值和最小值

--- src/Arraylist/ArraySuiJiShu.java
+++ src/Arraylist/ArraySuiJiShu.java
@@ -1,10 +1,28 @@
 package Arraylist;
 //求 10个两位的随机数的最大值，最小值，综合平均数
 public class ArraySuiJiShu {
-    public static void main(String[] args){
-          int[] arr = new int[10];
-          for(int i =0 ;i<arr.length;i++){
-              arr[i]=(int)Math.random() *(99 -10 +1)+10;
-          }
-    }
+    public static void main(String[] args)
+{
+        int[] arr = new int[10];
+        for(int i =0 ;i<arr.length;i++){
+        arr[i]=(int)(Math.random() *(99 -10 +1)+10);
+        }
+        //  求数组中元素最大值
+        int maxValue = 0;
+        for(int i = 1;i<arr.length;i++){
+            if(maxValue <arr[i]){
+                maxValue = arr[i];
+            }
+        }
+        System.out.println("最大值为" + maxValue);
+
+    //  求数组中元素最小值
+       int minValue = 0;
+        for(int i = 1;i<arr.length;i++){
+            if(minValue > arr[i]){
+                minValue = arr[i];
+            }
+        }
+        System.out.println("最大值为" + minValue);
 }
+                         }
