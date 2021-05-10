19d6c9bfb89a21b2b510e8ed7f0c7fa297965d65
zhanghongyu <843409677@qq.com>
2021-05-10 21:26:26 +0800

    再次反转

--- src/Arraylist/ArraryFanZhuan.java
+++ src/Arraylist/ArraryFanZhuan.java
@@ -6,7 +6,10 @@ public class ArraryFanZhuan {
         String[] arr1= new String[arr.length];
         System.out.println("输出第一位索引:");
         System.out.println(arr[0]);
-        //方法一
+        /**
+         * 方法一
+         */
+
         for(int i=0;i<arr.length/2;i++){
            String temp =arr[i];
            //第一位是0
@@ -19,5 +22,20 @@ public class ArraryFanZhuan {
             System.out.print(arr[i]+"\t");
         }
 
+        /**
+         * 方法2：
+         * 因为i的索引被定义为0，整体的长度为5。表示j是第5个长度，实际上索引位置是4，表示最后一个索引需要用整体的长度减1
+         * 但如果，i=1,那么j的所在位置就要减2
+         */
+        for(int i=0,j = arr.length-1;i<j;i++,j--){
+            String temp =arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+        }
+        System.out.println();
+        System.out.println("将反转后的数组再次反转:");
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+"\t");
     }
+}
 }
