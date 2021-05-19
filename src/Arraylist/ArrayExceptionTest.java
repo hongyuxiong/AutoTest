aecdd126905d1b6ccaac8177d6f58eabb849be38
zhanghongyu <843409677@qq.com>
2021-05-19 22:02:25 +0800

    空指针的例子

--- src/Arraylist/ArrayExceptionTest.java
+++ src/Arraylist/ArrayExceptionTest.java
@@ -22,7 +22,23 @@ public class ArrayExceptionTest {
             System.out.println(arr[i]);
         }
 
+          //
 
+        /**
+         * 空指针报错一般都是指针指向null值
+         *
+         * 1.某个元素为null或者整个数组都是null
+         int[]  arr1 = new int[]{1,2,3};
+         arr1[1] = Integer.parseInt(null);//这个函数可以强转？找个时间搜一下
+         arr1 =null;
+         System.out.println(arr1[1]);
+
+
+         2.二维数组赋值的问题
+         *  int[][] arr2 = new int[4][];
+         *  System.out.println(arr2[0][]);
+         *  外层元素是null，没有地址指向内层元素，所以是空指针
+         */
 
 
     }
